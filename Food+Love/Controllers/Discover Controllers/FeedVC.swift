7b6ee072b51b341ca5fe57401ff6d4c6048c76fd
winstonmaragh@ac.c4q.nyc <WinstonMaragh@ac.c4q.nyc>
2018-03-23 21:59:38 -0400

    changed Storyboard structure. Separated Storyboard into separate files to avoid merge conflicts. Embeded each storyboard in a separate group to avoid accidental touching of the storyboard file.

---
+++ Food+Love/Controllers/Discover Controllers/FeedVC.swift
@@ -0,0 +1,45 @@
+//
+//  MainFeedViewController.swift
+//  Food+Love
+//
+//  Created by Gloria Washington on 3/21/18.
+//  Copyright © 2018 Gloria Washington. All rights reserved.
+//
+
+
+import UIKit
+import Parchment
+import Firebase
+
+
+class FeedVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let discoverVC = UIViewController.storyboardInstance(storyboardName: "Discover", viewControllerIdentifiier: "DiscoverVC")
+		let admirersVC = UIViewController.storyboardInstance(storyboardName: "Discover", viewControllerIdentifiier: "AdmirersVC")
+		let pagingViewController = FixedPagingViewController(viewControllers: [discoverVC, admirersVC])
+		addChildViewController(pagingViewController)
+		view.addSubview(pagingViewController.view)
+		pagingViewController.didMove(toParentViewController: self)
+		pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			pagingViewController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+			pagingViewController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+			pagingViewController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+			pagingViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+			])
+
+		pagingViewController.menuBackgroundColor = UIColor.black
+		pagingViewController.backgroundColor = UIColor.black
+		pagingViewController.selectedBackgroundColor = UIColor.black
+		pagingViewController.indicatorColor = UIColor.white
+		pagingViewController.selectedTextColor = UIColor.red
+		pagingViewController.textColor = UIColor.white
+	}
+
+
+}
+
+
