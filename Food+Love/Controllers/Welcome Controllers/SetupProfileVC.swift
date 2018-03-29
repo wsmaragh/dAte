280e4ac29771b0a23b54f5b3be6ceb36fa43386c
winstonmaragh@ac.c4q.nyc <WinstonMaragh@ac.c4q.nyc>
2018-03-29 14:23:02 -0400

    update Welcome storyboard and setupProfile

--- Food+Love/Controllers/Welcome Controllers/SetupProfileVC.swift
+++ Food+Love/Controllers/Welcome Controllers/SetupProfileVC.swift
@@ -21,7 +21,6 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 	@IBOutlet var aboutSlide: AboutProfile!
 	@IBOutlet var signupSlide: UIView!
 	@IBOutlet var videoSlide: UIView!
-
 	@IBOutlet weak var actionButton: UIButton!
 
 	//Properties Fields
@@ -33,9 +32,14 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 	@IBOutlet weak var genderPreferenceSC: UISegmentedControl!
 	@IBOutlet weak var dobPicker: UIDatePicker!
 	@IBOutlet weak var zipcodeTF: UITextField!
-
+	@IBOutlet weak var firstNameTF: UITextFieldX!
+	@IBOutlet weak var emailTF: UITextFieldX!
+	@IBOutlet weak var passwordTF: UITextFieldX!
+	@IBOutlet weak var profileImageButton: UIButton!
+	@IBOutlet weak var bioTV: UITextView!
 
 	// MARK: Properties
+	private var imagePicker = UIImagePickerController()
 	var currentUser: User?
 	private var profileSlides = [UIView]()
 	private var slideIndex = 0
@@ -45,12 +49,14 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .white
+		imagePicker.delegate = self
 		currentUser = Auth.auth().currentUser
 		profileScrollView.delegate = self
-		profileSlides = [preferenceSlide, aboutSlide, signupSlide]
+		profileSlides = [preferenceSlide, aboutSlide, signupSlide, videoSlide]
 		addSlidesToScrollView(slides: profileSlides)
 		setupPageControl()
 		navigationController?.title = "Setup Profile"
+//		addShadeView()
 	}
 
 
@@ -60,6 +66,13 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 	}
 
 
+	fileprivate func addShadeView(){
+		let shade = UIView(frame: self.view.frame)
+		shade.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+		view.addSubview(shade)
+		view.sendSubview(toBack: shade)
+	}
+	
 	// MARK: Helper Methods
 	func addSlidesToScrollView(slides: [UIView]) {
 		profileScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: profileScrollView.bounds.height)
@@ -76,6 +89,31 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 
 	}
 
+	// MARK: Helper Methods
+	private func setupTextFields(){
+		//Underline
+		self.firstNameTF.underlined(color: .white)
+		self.emailTF.underlined(color: .white)
+		self.passwordTF.underlined(color: .white)
+		//LeftViewMode
+		self.firstNameTF.leftViewMode = .always
+		self.emailTF.leftViewMode = .always
+		self.passwordTF.leftViewMode = .always
+		//Text Color
+		firstNameTF.textColor = .white
+		emailTF.textColor = .white
+		passwordTF.textColor = .white
+		//Placeholder Color
+		if let firstNamePlaceholder = firstNameTF.placeholder {
+			firstNameTF.attributedPlaceholder = NSAttributedString(string: firstNamePlaceholder, attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightText])
+		}
+		if let emailPlaceholder = emailTF.placeholder {
+			emailTF.attributedPlaceholder = NSAttributedString(string: emailPlaceholder, attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightText])
+		}
+		if let passwordPlaceholder = passwordTF.placeholder {
+			passwordTF.attributedPlaceholder = NSAttributedString(string: passwordPlaceholder, attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightText])
+		}
+	}
 
 
 	// MARK: Action
@@ -111,24 +149,69 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 
 	@IBAction func nextPage(_ sender: UIButton) {
 		let count = profileSlides.count
-		if slideIndex < count {
-			let x = profileScrollView.contentOffset.x * 2
+		if slideIndex < count - 1 {
 			let currentPage = profileScrollView.contentOffset.x / profileScrollView.frame.size.width
 			let point = CGPoint(x: view.bounds.width * (currentPage + 1), y: 0)
 			profileScrollView.setContentOffset(point, animated: true)
 			pageControl.currentPage = Int(currentPage)
-			slideIndex = pageControl.currentPage
+			slideIndex = pageControl.currentPage + 1
+			print("Slide index:", slideIndex)
+			print("pageControl.currentPage:", pageControl.currentPage)
 		}
 
-		if slideIndex == count {
-			actionButton.titleLabel?.text = "Complete"
-			
+		if slideIndex == count - 1 {
+			actionButton.setTitle("Complete", for: .normal)
+			print(slideIndex)
+			print("complete profile now")
 		}
 
+		if actionButton.title(for: .normal) == "Complete" && pageControl.currentPage == 3{
+			print("create account")
+			//create auth account
+			//add lover details to database
+			//transition to main
+		}
 
+	}
 
+	@IBAction func addProfileImage(_ sender: UIButton) {
+		let alertController = UIAlertController(title: "Add profile image", message: "", preferredStyle: UIAlertControllerStyle.alert)
+		let existingPhotoAction = UIAlertAction(title: "Choose Existing Photo", style: .default) { (alertAction) in
+			self.launchCamera(type: UIImagePickerControllerSourceType.photoLibrary)
+		}
+		let newPhotoAction = UIAlertAction(title: "Take New Photo", style: .default) { (alertAction) in
+			self.launchCamera(type: UIImagePickerControllerSourceType.camera)
+		}
+		alertController.addAction(existingPhotoAction)
+		alertController.addAction(newPhotoAction)
+		present(alertController, animated: true, completion: nil)
+	}
 
-}
+	
+	@objc private func createNewAccount() {
+		guard let name = self.firstNameTF.text, name != "" else {
+			showAlert(title: "Please enter a name", message: ""); return
+		}
+		guard let email = self.firstNameTF.text, email != "" else {
+			showAlert(title: "Please enter an email", message: ""); return
+		}
+		guard let password = self.passwordTF.text, password != "" else {
+			showAlert(title: "Please enter a valid password", message: ""); return
+		}
+		if profileImageButton.image(for: UIControlState.normal) == #imageLiteral(resourceName: "selfieCamera") {
+			showAlert(title: "Please add a profile image", message: ""); return
+		}
+		guard let image = self.profileImageButton.image(for: .normal) else {
+			showAlert(title: "Please add a profile image", message: ""); return
+		}
+		if email.contains(" ") {
+			showAlert(title: "No spaces allowed in email!", message: nil); return
+		}
+		if password.contains(" ") {
+			showAlert(title: "No spaces allowed in password!", message: nil); return
+		}
+		AuthUserService.manager.createUser(name: name, email: email, password: password, profileImage: image)
+	}
 
 	func completeProfile(){
 		//add user details to database
@@ -137,19 +220,16 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 		guard let favCat3 = favoriteFoodCategory3TF.text else {return}
 		guard let favRest = favoriteRestaurant.text else {return}
 		guard let zipcode = 	zipcodeTF.text else {return}
+		guard let bio = bioTV.text else {return}
+
 		let gender = genderSC.selectedSegmentIndex == 0 ? "Male" : "Female"
 		let genderPreference =  genderPreferenceSC.selectedSegmentIndex == 0 ? "Male" : "Female"
 		let dobDate = dobPicker.date
 		let dob = DBService.manager.formatDateforDOB(with: dobDate)
-		let bio = ""
 
 		DBService.manager.addLoverDetails(favCat1: favCat1, favCat2: favCat2, favCat3: favCat3, favRestaurant: favRest, zipcode: zipcode, gender: gender, genderPreference: genderPreference, dateOfBirth: dob, bio: bio)
 
-		//transition to mainVC
-		//		let mainVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainController")
-		//		if let window = UIApplication.shared.delegate?.window {
-		//			window?.rootViewController = mainVC
-		//		}
+
 		print("user details added")
 	}
 
@@ -161,5 +241,65 @@ class SetupProfileVC: UIViewController, UIScrollViewDelegate {
 		slideIndex = pageControl.currentPage
 	}
 
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.emailTF.resignFirstResponder()
+		self.passwordTF.resignFirstResponder()
+		self.firstNameTF.resignFirstResponder()
+	}
+
+	private func showAlert(title: String, message: String?) {
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let okAction = UIAlertAction(title: "Ok", style: .default) {alert in }
+		alertController.addAction(okAction)
+		present(alertController, animated: true, completion: nil)
+	}
+
+	//Camera
+	func launchCamera(type: UIImagePickerControllerSourceType){
+		if UIImagePickerController.isSourceTypeAvailable(type){
+			imagePicker.sourceType = type
+			imagePicker.allowsEditing = true
+			self.present(imagePicker, animated: true, completion: nil)
+		}
+	}
+	
 
 }
+
+
+// MARK: TextField Delegate
+extension SetupProfileVC: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+}
+
+
+// MARK: Image Picker
+extension SetupProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		var selectedImageFromPicker: UIImage?
+		print("in image picker")
+		if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+			selectedImageFromPicker = editedImage
+			print("Edited image selected from library/camera")
+		}
+		else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+			selectedImageFromPicker = originalImage
+			print("original image selected from library/camera")
+		}
+		if let selectedImage = selectedImageFromPicker {
+			profileImageButton.setImage(selectedImage, for: .normal)
+		}
+		imagePicker.dismiss(animated: true, completion: nil)
+	}
+
+	//Cancel camera
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		imagePicker.dismiss(animated: true, completion: nil)
+	}
+
+}
+
+
